56b0e7f4e09d3d39125f1ea967a92564e8f83475
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-10-09 06:37:41 -0700

    first 4 tasks

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
+        return None
+    else:
+        my_list[idx] = element
+        return my_lis2-replace_in_list.py2-replace_in_list.pyt
