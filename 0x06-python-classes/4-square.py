c803c5937e49d03fdda8357ea038658b353786bb
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-10-24 10:40:04 -0700

    square task

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Define Square class"""
+
+
+class Square:
+    """Square Class"""
+
+    def __init__(self, size=0):
+        """Initialize Square
+        Args:
+            size (int): size of square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """get method"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Method to compute area of square
+        Args: none
+        Return: area of square
+        """
+        return (self.__size * self.__size)
