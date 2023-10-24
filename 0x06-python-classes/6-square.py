b84f171e3077fa948e646f08f1388c5ee886e806
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-10-24 12:11:19 -0700

    square task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,10 +46,11 @@ class Square:
 
     def my_print(self):
         """function to print # in square"""
-        if self.size == 0:
+        if self.__size == 0:
             print("")
             return
 
+        [print("") for i in range(0, __position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for k in range(0, self.__position[0])]
             [print("#", end="") for j in range(0, self.__size)]
