2218b0c1b6b38c06ef83d144ff0b42752bbac16a
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-11-01 04:33:22 -0700

    doctesting

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a function"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Function printing My name is <first name> <last name>
+
+    Args:
+        first_name (str): first name
+        lasst_name (str): second name
+
+    Raises:
+        TypeError
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
