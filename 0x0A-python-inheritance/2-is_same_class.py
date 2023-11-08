5adad034c13af6997a49e19f6e9a29c174392226
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-11-08 00:12:19 -0800

    inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines a function"""
+
+
+def is_same_class(obj, a_class):
+    """ Function to check if an instance is in a class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
