643fea3eb00470556ac37e2ea82369d9beb68ca6
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-11-08 03:13:28 -0800

    inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -4,7 +4,6 @@
 
 class BaseGeometry:
     """ Base geometry class"""
-    
     def area(self):
         """ area method that raises an exception"""
         raise Exception("area() is not implemented")
