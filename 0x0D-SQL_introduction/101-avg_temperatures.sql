e2e728b39820e55ddad724ba81e79e05b5b8ddc4
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-12-12 11:16:44 -0800

    SQL intro

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- displays average temprature
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,2 @@
+-- Lists max value by state
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
