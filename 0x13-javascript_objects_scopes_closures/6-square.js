651c160d951f3ae5551729f63a43fd1f8f7deb66
BirtukanK <birtukankuma1113@gmail.com>
2024-01-09 09:48:05 -0800

    square class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const Square = class Square extends require('./5-square.js') {
+  constructor (size) {
+    super();
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      for (let i = 0; i < this.size; i++) {
+        let y = '';
+        for (let j = 0; j < this.size; j++) {
+          y += 'X';
+        }
+        console.log(y);
+      }
+    } else {
+      for (let i = 0; i < this.size; i++) {
+        let x = '';
+        for (let j = 0; j < this.size; j++) {
+          x += 'c';
+        }
+        console.log(x);
+      }
+    }
+  }
+};
+module.exports = Square;
