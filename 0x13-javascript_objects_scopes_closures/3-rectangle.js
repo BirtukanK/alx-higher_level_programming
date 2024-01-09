c8f789ceebee3d978cf51d3109fba5ad9349ec9a
BirtukanK <birtukankuma1113@gmail.com>
2024-01-09 03:05:42 -0800

    js class

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Rectangle = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i <= this.width; i++) {
+      for (let j = 0; j <= this.height; j++) {
+        console.log('X');
+	console.log('\n');
+      }
+      console.log('\n');
+    }
+  }
+};
+module.exports = Rectangle;
