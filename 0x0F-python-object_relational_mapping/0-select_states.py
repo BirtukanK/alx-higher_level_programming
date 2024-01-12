bedb184424ea1c39bce442b916a620733f84a3f4
Birtukan Kuma <birtukankuma1113@gmail.com>
2024-01-11 22:52:48 -0800

    fix query

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" Defines a script to list items"""
+# Defines a script to list items
+import sys
 import MySQLdb
-""" importing important modules"""
 
-conn = MySQLdb.connect(host="localhost", port=3306,
-                       user="root", passwd="root", db="hbtn_0e_0_usa")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC;")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user="root", passwd="root", db="hbtn_0e_0_usa")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
