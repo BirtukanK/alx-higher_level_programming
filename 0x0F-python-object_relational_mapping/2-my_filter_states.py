65b48c5ae1d99d716ad1f69daeb2c35431dfd7a6
Birtukan Kuma <birtukankuma1113@gmail.com>
2024-01-12 00:14:58 -0800

    task2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Defines a script to list items"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC;"
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cur.close()
+    conn.close()
