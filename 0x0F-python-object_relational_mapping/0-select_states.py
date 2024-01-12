0906fbce6e9e603a2cfde74cab87759308f5f6b2
Birtukan Kuma <birtukankuma1113@gmail.com>
2024-01-11 23:39:09 -0800

    fix first

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-# Defines a script to list items
+""" Defines a script to list items"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host="localhost", port=3306)
     cur = conn.cursor()
     cur.execute("SELECT * FROM `states`")
     [print(state) for state in cur.fetchall()]
+
+    cur.close()
+    conn.close()
