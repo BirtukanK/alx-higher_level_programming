a3e4059b516de61cd3732f005f098aec84c4ea98
Birtukan Kuma <birtukankuma1113@gmail.com>
2024-01-11 23:48:51 -0800

    fix task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """ Defines a script to list items"""
 import MySQLdb
-""" importing important modules"""
+import sys
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ;")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ;")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
