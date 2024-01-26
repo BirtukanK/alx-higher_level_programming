2d15dd0e9af764ca183a357efb8c53803f49a296
BirtukanK <birtukankuma1113@gmail.com>
2024-01-26 04:20:16 -0800

    fetching url

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" takes URL and display the value of X-Request-Id"""
+import sys
+import urllib.request
+with urllib.request.urlopen(sys.argv[1]) as response:
+   html = response.getheader("X-Request-Id")
+   print(html)
