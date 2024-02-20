9bacbe2be50370568f85cd8d70ac53eaebc9227c
BirtukanK <birtukankuma1113@gmail.com>
2024-02-20 13:56:44 -0800

    standard fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,8 +5,11 @@ const args = process.argv;
 const endpoint = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(args[2], function (error, response, body) {
-	const filmdata = JSON.parse(body);
-	const moviesBy18 = filmdata.results.filter(film =>
-	film.characters.includes(endpoint));
-	console.log(moviesBy18.length);
+  if (error) {
+    console.error(error);
+  }
+  const filmdata = JSON.parse(body);
+  const moviesBy18 = filmdata.results.filter(film =>
+    film.characters.includes(endpoint));
+  console.log(moviesBy18.length);
 });
