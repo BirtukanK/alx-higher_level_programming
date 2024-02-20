51b64ae364e28587e5a839da35def9129559c218
BirtukanK <birtukankuma1113@gmail.com>
2024-02-20 03:49:10 -0800

    fix js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const args = process.argv;
-const endpoint = 'https://swapi-api.alx-tools.com/api/films/:' + args[2];
 
-request(endpoint, funcion (err, response) {
-	console.error(err);
-	console.log(response);
+const args = process.argv;
+const endpoint = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
+
+request(endpoint, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
