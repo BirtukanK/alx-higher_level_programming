d497d0f58fc53f243b2cfb4e0b77586f8264940b
BirtukanK <birtukankuma1113@gmail.com>
2024-02-20 05:52:15 -0800

    starwars count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+
+const args = process.argv;
+const endpoint = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request(args[2], function (error, response, body) {
+	const filmdata = JSON.parse(body);
+	const moviesBy18 = filmdata.results.filter(film =>
+	film.characters.includes(endpoint));
+	console.log(moviesBy18.length);
+});
