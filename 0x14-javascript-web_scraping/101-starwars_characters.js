93a3f9089b7dae850fa12359179110de0b97c30d
BirtukanK <birtukankuma1113@gmail.com>
2024-02-20 16:36:04 -0800

    advanced task

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+request(url, function (error, reponse, body) {
+  if (error) {
+    console.error(error);
+  }
+  const filmdatas = JSON.parse(body);
+  request(filmdatas, function (error, reponse, body) {
+    if (error) {
+      console.error(error);
+    }
+    const filmcharacter = JSON.parse(body);
+    const endpoints = filmcharacter.characters;
+    endpoints.forEach((endpoint) => {
+      request(endpoint, function (error, reponse, body) {
+        if (error) {
+          console.error(error);
+        }
+        const characterdata = JSON.parse(body);
+        console.log(characterdata.name);
+      });
+    });
+  });
+}); 
